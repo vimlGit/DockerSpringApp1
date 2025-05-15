9f7543dc49957dd495f7d89cd09bfc77cf58f90a
Dev2 <Dev2@gmail.com>
2025-05-15 09:40:02 +0530

    initial commit

---
+++ DockerSBootApp1/src/main/java/com/vk/DockerSBootApp1Application.java
@@ -0,0 +1,14 @@
+package com.vk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSBootApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSBootApp1Application.class, args);
+		System.out.println("Welcome Spring Boot app...,vimlesh");
+	}
+
+}
